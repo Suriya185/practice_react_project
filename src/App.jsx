56428818f70828e6ae2a4ee215bf56ecc58d95f1
Suriya185 <suriya.coder185@gmail.com>
2025-03-02 12:19:52 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import MainContent from "./components/MainContent";
+import logo from "./assets/images/logo.png";
+
+function App() {
+  console.log("Testing lint errors");
+  return (
+    <>
+      <header>
+        <div id="logo-img">
+          <img src={logo} />
+        </div>
+        <h1>Learn & Master GitHub Actions</h1>
+      </header>
+      <MainContent />
+    </>
+  );
+}
+
+export default App;
